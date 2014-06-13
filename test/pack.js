7959a5afb7ac7dba2cdecf65ead940e98bc8ac8e
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-13 17:43:30 +1000

    more tests - looking good

--- test/pack.js
+++ test/pack.js
@@ -10,3 +10,23 @@ test('0.0.1', function(t) {
   t.plan(1);
   t.equal(key(t.name), 'P0281474976710654');
 });
+
+test('0.1.0', function(t) {
+  t.plan(1);
+  t.equal(key(t.name), 'P0281474976645119');
+});
+
+test('1.0.0', function(t) {
+  t.plan(1);
+  t.equal(key(t.name), 'P0281470681743359');
+});
+
+test('1.0.1', function(t) {
+  t.plan(1);
+  t.equal(key(t.name), 'P0281470681743358');
+});
+
+test('123123.2343.344', function(t) {
+  t.plan(1);
+  t.equal(key(t.name), 'P0034140541484711');
+});
